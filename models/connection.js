03cd8ee6180436365003c5f00be49f08f022d956
cédric <cedric_lecalvez@hotmail.com>
2021-02-11 15:06:18 +0100

    changement bdd

--- models/connection.js
+++ models/connection.js
@@ -6,7 +6,7 @@ var options = {
     useNewUrlParser: true,
 }
 
-mongoose.connect('mongodb+srv://ced:cedric@cluster0.a4s7d.mongodb.net/morningnews?retryWrites=true&w=majority',
+mongoose.connect(process.env.MONGODB,
     options,
     function(err){
         console.log(err);
